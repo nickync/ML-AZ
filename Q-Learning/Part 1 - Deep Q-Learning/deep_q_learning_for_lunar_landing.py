325344dd1d4468d015f59a348914c637ca3ffe80
nickync <lizheng9024@gmail.com>
2024-10-12 22:27:33 -0400

    train

--- Q-Learning/Part 1 - Deep Q-Learning/deep_q_learning_for_lunar_landing.py
+++ Q-Learning/Part 1 - Deep Q-Learning/deep_q_learning_for_lunar_landing.py
@@ -72,8 +72,8 @@ class ReplayMemory(object):
         actions = torch.from_numpy(np.vstack([e[1] for e in experiences if e is not None])).long().to(self.device)
         rewards = torch.from_numpy(np.vstack([e[2] for e in experiences if e is not None ])).float().to(self.device)
         next_states = torch.from_numpy(np.vstack([e[3] for e in experiences if e is not None])).float().to(self.device)
-        dones = torch.from_numpy(np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)) .float().to(self.device)
-        return states, actions, rewards, next_states, dones
+        dones = torch.from_numpy(np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
+        return states, next_states, actions, rewards, dones
         
 
 # implement the DQN class
@@ -94,8 +94,8 @@ class Agent():
         self.t_step = (self.t_step + 1) % 4
         if self.t_step == 0:
             if len(self.memory.memory) > minibatch_size:
-                experiences = self.memory.sample()
-                self.learn(experiences, discount_factor, )
+                experiences = self.memory.sample(100)
+                self.learn(experiences, discount_factor)
 
     def act(self, state, epsilon = 0.):
         """ local Q network returns actions and choose final action base on the epsilon """
@@ -114,7 +114,7 @@ class Agent():
         """ uses experiences from replay memory to update the local Q network's Q value towards the target Q network """
         states, next_states, actions, rewards, dones = experiences
         next_q_targets = self.target_qnetwork(next_states).detach().max(1)[0].unsqueeze(1)
-        q_targets = rewards + (discount_factor * next_q_targets * (1 - dones))
+        q_targets = rewards + discount_factor * next_q_targets * (1 - dones)
         q_expected = self.local_qnetwork(states).gather(1, actions)
         loss = F.mse_loss(q_expected, q_targets)
         self.optimizer.zero_grad()
@@ -124,7 +124,7 @@ class Agent():
 
     def soft_update(self, local_model, target_model, interpolation_parameter):
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(interpolation_parameter * local_param.data + (1.0 - interpolation_parameter) * target_param)
+            target_param.data.copy_(interpolation_parameter * local_param.data + (1.0 - interpolation_parameter) * target_param.data)
     
 
 # Tarining the DQN agent
@@ -132,7 +132,7 @@ class Agent():
 if __name__ == '__main__':
     # set up 
     import gymnasium as gym
-    env = gym.make('LunarLander-v3')
+    env = gym.make('LunarLander-v2')
     state_shape = env.observation_space.shape
     state_size = env.observation_space.shape[0]
     number_actions = env.action_space.n
