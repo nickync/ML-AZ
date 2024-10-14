b736352041ec94addd2988038598c60d9e7de940
nickync <lizheng9024@gmail.com>
2024-10-13 22:42:12 -0400

    eval

--- Q-Learning/Part 2 - CNN Deep Q-Learning/deep_q_convolutional_pacman.py
+++ Q-Learning/Part 2 - CNN Deep Q-Learning/deep_q_convolutional_pacman.py
@@ -131,75 +131,75 @@ class Agent():
     self.optimizer.step()
 
 # Initializing the DCQN agent
+if __name__ == '__main__':
+    agent = Agent(number_actions)
 
-agent = Agent(number_actions)
+    # Training the DCQN agent
 
-# Training the DCQN agent
+    number_episodes = 2000
+    maximum_number_timesteps_per_episode = 10000
+    epsilon_starting_value  = 1.0
+    epsilon_ending_value  = 0.01
+    epsilon_decay_value  = 0.995
+    epsilon = epsilon_starting_value
+    scores_on_100_episodes = deque(maxlen = 100)
 
-number_episodes = 2000
-maximum_number_timesteps_per_episode = 10000
-epsilon_starting_value  = 1.0
-epsilon_ending_value  = 0.01
-epsilon_decay_value  = 0.995
-epsilon = epsilon_starting_value
-scores_on_100_episodes = deque(maxlen = 100)
+    for episode in range(1, number_episodes + 1):
+        state, _ = env.reset()
+        score = 0
+        for t in range(maximum_number_timesteps_per_episode):
+            action = agent.act(state, epsilon)
+            next_state, reward, done, _, _ = env.step(action)
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break
+        scores_on_100_episodes.append(score)
+        epsilon = max(epsilon_ending_value, epsilon_decay_value * epsilon)
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode, np.mean(scores_on_100_episodes)), end = "")
+        if episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode, np.mean(scores_on_100_episodes)))
+        if np.mean(scores_on_100_episodes) >= 800.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(episode - 100, np.mean(scores_on_100_episodes)))
+            torch.save(agent.local_qnetwork.state_dict(), 'checkpoint_pac.pth')
+            break
 
-for episode in range(1, number_episodes + 1):
-  state, _ = env.reset()
-  score = 0
-  for t in range(maximum_number_timesteps_per_episode):
-    action = agent.act(state, epsilon)
-    next_state, reward, done, _, _ = env.step(action)
-    agent.step(state, action, reward, next_state, done)
-    state = next_state
-    score += reward
-    if done:
-      break
-  scores_on_100_episodes.append(score)
-  epsilon = max(epsilon_ending_value, epsilon_decay_value * epsilon)
-  print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode, np.mean(scores_on_100_episodes)), end = "")
-  if episode % 100 == 0:
-    print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode, np.mean(scores_on_100_episodes)))
-  if np.mean(scores_on_100_episodes) >= 500.0:
-    print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(episode - 100, np.mean(scores_on_100_episodes)))
-    torch.save(agent.local_qnetwork.state_dict(), 'checkpoint.pth')
-    break
+    # Part 3 - Visualizing the results
 
-# Part 3 - Visualizing the results
+    import glob
+    import io
+    import base64
+    import imageio
+    from IPython.display import HTML, display
+    from gym.wrappers.monitoring.video_recorder import VideoRecorder
 
-import glob
-import io
-import base64
-import imageio
-from IPython.display import HTML, display
-from gym.wrappers.monitoring.video_recorder import VideoRecorder
+    def show_video_of_model(agent, env_name):
+        env = gym.make(env_name, render_mode='rgb_array')
+        state, _ = env.reset()
+        done = False
+        frames = []
+        while not done:
+            frame = env.render()
+            frames.append(frame)
+            action = agent.act(state)
+            state, reward, done, _, _ = env.step(action)
+        env.close()
+        imageio.mimsave('video.mp4', frames, fps=30)
 
-def show_video_of_model(agent, env_name):
-    env = gym.make(env_name, render_mode='rgb_array')
-    state, _ = env.reset()
-    done = False
-    frames = []
-    while not done:
-        frame = env.render()
-        frames.append(frame)
-        action = agent.act(state)
-        state, reward, done, _, _ = env.step(action)
-    env.close()
-    imageio.mimsave('video.mp4', frames, fps=30)
+    show_video_of_model(agent, 'MsPacmanDeterministic-v0')
 
-show_video_of_model(agent, 'MsPacmanDeterministic-v0')
+    def show_video():
+        mp4list = glob.glob('*.mp4')
+        if len(mp4list) > 0:
+            mp4 = mp4list[0]
+            video = io.open(mp4, 'r+b').read()
+            encoded = base64.b64encode(video)
+            display(HTML(data='''<video alt="test" autoplay
+                    loop controls style="height: 400px;">
+                    <source src="data:video/mp4;base64,{0}" type="video/mp4" />
+                </video>'''.format(encoded.decode('ascii'))))
+        else:
+            print("Could not find video")
 
-def show_video():
-    mp4list = glob.glob('*.mp4')
-    if len(mp4list) > 0:
-        mp4 = mp4list[0]
-        video = io.open(mp4, 'r+b').read()
-        encoded = base64.b64encode(video)
-        display(HTML(data='''<video alt="test" autoplay
-                loop controls style="height: 400px;">
-                <source src="data:video/mp4;base64,{0}" type="video/mp4" />
-             </video>'''.format(encoded.decode('ascii'))))
-    else:
-        print("Could not find video")
-
-show_video()
+    show_video()
---
+++ Q-Learning/Part 2 - CNN Deep Q-Learning/eval.py
@@ -0,0 +1,30 @@
+import torch
+import deep_q_convolutional_pacman as m
+import numpy as np
+import imageio
+
+
+state_dic = torch.load('checkpoint.pth')
+
+import gymnasium as gym
+env = gym.make('MsPacmanDeterministic-v4', render_mode='rgb_array', full_action_space = False)
+state_shape = env.observation_space.shape
+state_size = env.observation_space.shape[0]
+number_actions = env.action_space.n
+
+model = m.Network(number_actions)
+model.load_state_dict(state_dic)
+model.eval()
+
+state, _ = env.reset()
+done = False
+frames = []
+while not done:
+    frame = env.render()
+    frames.append(frame)
+    state = m.preprocess_frame(state)
+    actions = model(state)
+    action = np.argmax(actions.cpu().data.numpy())
+    state, reward, done, _, _ = env.step(action)
+env.close()
+imageio.mimsave('eval.mp4', frames, fps=30)
